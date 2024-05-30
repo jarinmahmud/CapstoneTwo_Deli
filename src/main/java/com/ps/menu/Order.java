9fdceb534d8d5f076e5ea15619ef4c20e22f5d84
Jarin Mahmud <jarinmahmud@gmail.com>
2024-05-30 13:21:36 -0400

    updated code

--- src/main/java/com/ps/menu/Order.java
+++ src/main/java/com/ps/menu/Order.java
@@ -1,8 +1,10 @@
 package com.ps.menu;
 
 import java.util.List;
+import java.io.Serializable;
 
-public class Order {
+
+public class Order implements Serializable {
     private List<Sandwich> sandwiches;
     private List<Drink> drinks;
     private List<Chip> chips;
@@ -13,34 +15,56 @@ public class Order {
         this.chips = chips;
     }
 
-    public double calculateTotalPrice() {
-        double total = 0;
-        for (Sandwich sandwich : sandwiches) {
-            total += sandwich.getTotalPrice();
-        }
-        for (Drink drink : drinks) {
-            total += drink.getPrice();
-        }
-        for (Chip chip : chips) {
-            total += chip.getPrice();
-        }
-        return total;
-    }
-
     @Override
     public String toString() {
-        StringBuilder sb = new StringBuilder();
-        sb.append("Order Details:\n");
-        for (Sandwich sandwich : sandwiches) {
-            sb.append(sandwich.toString()).append("\n");
-        }
-        for (Drink drink : drinks) {
-            sb.append(drink.toString()).append("\n");
-        }
-        for (Chip chip : chips) {
-            sb.append(chip.toString()).append("\n");
-        }
-        sb.append("Total Price: $").append(String.format("%.2f", calculateTotalPrice())).append("\n");
-        return sb.toString();
+        return "Order{" +
+                "sandwiches=" + sandwiches +
+                ", drinks=" + drinks +
+                ", chips=" + chips +
+                '}';
     }
 }
+
+
+//public class Order {
+//    private List<Sandwich> sandwiches;
+//    private List<Drink> drinks;
+//    private List<Chip> chips;
+//
+//    public Order(List<Sandwich> sandwiches, List<Drink> drinks, List<Chip> chips) {
+//        this.sandwiches = sandwiches;
+//        this.drinks = drinks;
+//        this.chips = chips;
+//    }
+//
+//    public double calculateTotalPrice() {
+//        double total = 0;
+//        for (Sandwich sandwich : sandwiches) {
+//            total += sandwich.getTotalPrice();
+//        }
+//        for (Drink drink : drinks) {
+//            total += drink.getPrice();
+//        }
+//        for (Chip chip : chips) {
+//            total += chip.getPrice();
+//        }
+//        return total;
+//    }
+//
+//    @Override
+//    public String toString() {
+//        StringBuilder sb = new StringBuilder();
+//        sb.append("Order Details:\n");
+//        for (Sandwich sandwich : sandwiches) {
+//            sb.append(sandwich.toString()).append("\n");
+//        }
+//        for (Drink drink : drinks) {
+//            sb.append(drink.toString()).append("\n");
+//        }
+//        for (Chip chip : chips) {
+//            sb.append(chip.toString()).append("\n");
+//        }
+//        sb.append("Total Price: $").append(String.format("%.2f", calculateTotalPrice())).append("\n");
+//        return sb.toString();
+//    }
+//}
--- src/main/java/com/ps/pos/POS.java
+++ src/main/java/com/ps/pos/POS.java
@@ -193,4 +193,3 @@ public class POS implements POSInterface {
         }
     }
 }
-
