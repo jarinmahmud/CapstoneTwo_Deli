938d5ba5a7a11ed165902fa62daf6d1fc41d16d5
Jarin Mahmud <jarinmahmud@gmail.com>
2024-05-30 17:32:56 -0400

    updated code on classes

--- src/main/java/com/ps/menu/Order.java
+++ src/main/java/com/ps/menu/Order.java
@@ -1,10 +1,8 @@
 package com.ps.menu;
 
 import java.util.List;
-import java.io.Serializable;
 
-
-public class Order implements Serializable {
+public class Order {
     private List<Sandwich> sandwiches;
     private List<Drink> drinks;
     private List<Chip> chips;
@@ -15,56 +13,34 @@ public class Order implements Serializable {
         this.chips = chips;
     }
 
+    public double calculateTotalPrice() {
+        double total = 0;
+        for (Sandwich sandwich : sandwiches) {
+            total += sandwich.getTotalPrice();
+        }
+        for (Drink drink : drinks) {
+            total += drink.getPrice();
+        }
+        for (Chip chip : chips) {
+            total += chip.getPrice();
+        }
+        return total;
+    }
+
     @Override
     public String toString() {
-        return "Order{" +
-                "sandwiches=" + sandwiches +
-                ", drinks=" + drinks +
-                ", chips=" + chips +
-                '}';
+        StringBuilder sb = new StringBuilder();
+        sb.append("Order Details:\n");
+        for (Sandwich sandwich : sandwiches) {
+            sb.append(sandwich.toString()).append("\n");
+        }
+        for (Drink drink : drinks) {
+            sb.append(drink.toString()).append("\n");
+        }
+        for (Chip chip : chips) {
+            sb.append(chip.toString()).append("\n");
+        }
+        sb.append("Total Price: $").append(String.format("%.2f", calculateTotalPrice())).append("\n");
+        return sb.toString();
     }
 }
-
-
-//public class Order {
-//    private List<Sandwich> sandwiches;
-//    private List<Drink> drinks;
-//    private List<Chip> chips;
-//
-//    public Order(List<Sandwich> sandwiches, List<Drink> drinks, List<Chip> chips) {
-//        this.sandwiches = sandwiches;
-//        this.drinks = drinks;
-//        this.chips = chips;
-//    }
-//
-//    public double calculateTotalPrice() {
-//        double total = 0;
-//        for (Sandwich sandwich : sandwiches) {
-//            total += sandwich.getTotalPrice();
-//        }
-//        for (Drink drink : drinks) {
-//            total += drink.getPrice();
-//        }
-//        for (Chip chip : chips) {
-//            total += chip.getPrice();
-//        }
-//        return total;
-//    }
-//
-//    @Override
-//    public String toString() {
-//        StringBuilder sb = new StringBuilder();
-//        sb.append("Order Details:\n");
-//        for (Sandwich sandwich : sandwiches) {
-//            sb.append(sandwich.toString()).append("\n");
-//        }
-//        for (Drink drink : drinks) {
-//            sb.append(drink.toString()).append("\n");
-//        }
-//        for (Chip chip : chips) {
-//            sb.append(chip.toString()).append("\n");
-//        }
-//        sb.append("Total Price: $").append(String.format("%.2f", calculateTotalPrice())).append("\n");
-//        return sb.toString();
-//    }
-//}
